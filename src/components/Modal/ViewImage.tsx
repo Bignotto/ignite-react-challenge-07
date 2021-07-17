97f7948305887bf00ade1a5f908a3e2e620aed59
Thiago Bignotto <bignotto@gmail.com>
2021-07-17 17:20:19 -0300

    save commit

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,29 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
+  const handleCloseModal = (): void => {
+    onClose();
+  };
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={handleCloseModal}
+      isCentered
+      size="100%"
+      px={60}
+    >
+      <ModalOverlay />
+      <ModalContent bgColor="pGray.900">
+        <ModalBody px={60}>
+          <Image
+            src={imgUrl}
+            objectFit="cover"
+            borderTopRadius="md"
+            cursor="pointer"
+          />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
 }
